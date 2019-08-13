38c89be55981f8bf8761fe0bc5521b3e7a6153df
frosteryd <linus.frosteryd@gmail.com>
2019-08-13 13:23:01 +0200

    init

---
+++ initial_setup.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import setup_lib
+
+
+if os.getuid():
+    sys.exit('You need root access to install!')
+
+
+os.system('clear')
+print()
+print()
+print("###################################")
+print("##### RaspiWiFi Intial Setup  #####")
+print("###################################")
+print()
+print()
+entered_ssid = "SL-Screen"
+wpa_enabled_choice = "N"
+wpa_entered_key = "coolcool"
+auto_config_choice = "y"
+auto_config_delay = "100"
+server_port_choice = "80"
+ssl_enabled_choice = "N"
+install_ans = input("Are you ready to commit changes to the system? [y/N]: ")
+
+if(install_ans.lower() == 'y'):
+	setup_lib.install_prereqs()
+	setup_lib.copy_configs(wpa_enabled_choice)
+	setup_lib.update_main_config_file(entered_ssid, auto_config_choice, auto_config_delay, ssl_enabled_choice, server_port_choice, wpa_enabled_choice, wpa_entered_key)
+else:
+	print()
+	print()
+	print("===================================================")
+	print("---------------------------------------------------")
+	print()
+	print("RaspiWiFi installation cancelled. Nothing changed...")
+	print()
+	print("---------------------------------------------------")
+	print("===================================================")
+	print()
+	print()
+	sys.exit()
+
+os.system('clear')
+print()
+print()
+print("#####################################")
+print("##### RaspiWiFi Setup Complete  #####")
+print("#####################################")
+print()
+print()
+print("Initial setup is complete. A reboot is required to start in WiFi configuration mode...")
+reboot_ans = input("Would you like to do that now? [y/N]: ")
+
+if reboot_ans.lower() == 'y':
+	os.system('reboot')
