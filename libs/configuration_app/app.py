79c1655b8f415d52c0058fcc5e6dd911231e78f8
frosteryd <linus.frosteryd@gmail.com>
2019-08-22 15:21:05 +0200

    fix bug in the making of wpa_supplicant

--- libs/configuration_app/app.py
+++ libs/configuration_app/app.py
@@ -121,7 +121,7 @@ def scan_wifi_networks():
 
 def create_wpa_supplicant(ssid, wifi_key):
     temp_conf_file = open('wpa_supplicant.conf.tmp', 'w')
-    temp_conf_file.write('country=SE') #TODO make this dynamic if we ship to more countries
+    temp_conf_file.write('country=SE\n') #TODO make this dynamic if we ship to more countries
     temp_conf_file.write('ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n')
     temp_conf_file.write('update_config=1\n')
     temp_conf_file.write('\n')
@@ -133,7 +133,7 @@ def create_wpa_supplicant(ssid, wifi_key):
     else:
         temp_conf_file.write('	psk="' + wifi_key + '"\n')
 
-    temp_conf_file.write('	}')
+    temp_conf_file.write('	}\n')
 
     temp_conf_file.close
 
